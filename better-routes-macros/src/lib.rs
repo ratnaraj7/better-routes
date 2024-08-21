9b62f03835e4563891de739cee49cf6a3f1f3ec6
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-08-21 07:43:48 +0000

    fix: add error for #[method_helper] with args

--- better-routes-macros/src/lib.rs
+++ better-routes-macros/src/lib.rs
@@ -2,6 +2,7 @@ mod method_helper;
 mod routes;
 
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
 use syn::parse_macro_input;
 
@@ -10,7 +11,15 @@ use crate::routes::Routes;
 use self::method_helper::MethodHelper;
 
 #[proc_macro_attribute]
-pub fn method_helper(_attr: TokenStream, input: TokenStream) -> TokenStream {
+pub fn method_helper(attr: TokenStream, input: TokenStream) -> TokenStream {
+    if !attr.is_empty() {
+        return syn::Error::new(
+            Span::call_site(),
+            "#[method_helper] attribute takes no arguments",
+        )
+        .to_compile_error()
+        .into();
+    }
     let method_helper = parse_macro_input!(input as MethodHelper);
     quote! {
         #method_helper
