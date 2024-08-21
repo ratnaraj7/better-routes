18f5cc6a7f4a6777e13ccee8a7c16b43c050fb2f
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-08-21 07:39:19 +0000

    docs: update example

--- better-routes/src/lib.rs
+++ better-routes/src/lib.rs
@@ -22,14 +22,16 @@
 //! ```rust,no_run
 //! use better_routes::{routes, method_helper};
 //! use axum_extra::routing::RouterExt;
-//! use axum::Router;
-//! use axum::http::StatusCode;
-//! use axum::response::IntoResponse;
-//! use axum::extract::rejection::PathRejection;
-//! use axum::Json;
+//! use serde::Deserialize;
+//! use axum::{
+//!     Router,
+//!     Json,
+//!     http::StatusCode,
+//!     response::IntoResponse,
+//!     extract::rejection::PathRejection,
+//! };
 //!
 //! // Define a custom rejection type for handling global rejections
-//! #[derive(Default)]
 //! struct GlobalRejection;
 //!
 //! impl From<PathRejection> for GlobalRejection {
@@ -46,7 +48,6 @@
 //! }
 //!
 //! // Define a custom rejection type for handling user-specific rejections
-//! #[derive(Default)]
 //! struct UserRejection;
 //!
 //! impl From<PathRejection> for UserRejection {
@@ -66,19 +67,35 @@
 //! #[derive(Clone)]
 //! struct AppState;
 //!
+//! // Define a struct for products
+//! #[derive(Deserialize)]
+//! struct Product {
+//!     id: usize,
+//! }
+//!
+//! // Define a struct for users
+//! #[derive(Deserialize)]
+//! struct User {
+//!     id: usize,
+//! }
+//!
 //! // Use the `routes!` macro to define routes and associate them with handlers and rejections
+//! // The `name` specified here, `AllRoutes`, will be used to create a struct
+//! // by the `routes!` macro. This struct implements the `routes()` method,
+//! // which generates the router instance with all defined routes.
 //! routes! {
-//!     State => AppState, // Specify application state
-//!     Rejection => GlobalRejection, // Apply global rejection handler
-//!     "/product/:id" => struct Product { id: usize }, // Define route for products
-//!     "/users/:id" => struct Users { id: usize } => UserRejection, // Define route for users with custom rejection
+//!     name => pub AllRoutes, // You can also specify visibility for the generated struct
+//!     state => AppState, // Specify application state
+//!     rejection => GlobalRejection, // Apply global rejection handler
+//!     "/product/:id" => Product, // Define route for product
+//!     "/user/:id" => User => UserRejection, // Define route for user with custom rejection
 //! }
 //!
 //! // Implement handlers for the `Product` struct
-//! #[method_helper(AppState)]
+//! #[method_helper]
 //! impl Product {
 //!     #[get]
-//!     async fn get(self) -> Json<String> {
+//!     async fn get_product(self) -> Json<String> {
 //!         // Print the ID to the console
 //!         println!("Product ID: {}", self.id);
 //!         // Return a response with the ID
@@ -86,59 +103,59 @@
 //!     }
 //!
 //!     #[post]
-//!     async fn create(self) -> Json<String> {
+//!     async fn create_product(self) -> Json<String> {
 //!         println!("Creating product with ID: {}", self.id);
 //!         Json(format!("Created product with ID: {}", self.id))
 //!     }
 //!
 //!     #[put]
-//!     async fn update(self) -> Json<String> {
+//!     async fn update_product(self) -> Json<String> {
 //!         println!("Updating product with ID: {}", self.id);
 //!         Json(format!("Updated product with ID: {}", self.id))
 //!     }
 //!
 //!     #[delete]
-//!     async fn delete(self) -> Json<String> {
+//!     async fn delete_product(self) -> Json<String> {
 //!         println!("Deleting product with ID: {}", self.id);
 //!         Json(format!("Deleted product with ID: {}", self.id))
 //!     }
 //!
 //!     #[patch]
-//!     async fn patch(self) -> Json<String> {
+//!     async fn patch_product(self) -> Json<String> {
 //!         println!("Patching product with ID: {}", self.id);
 //!         Json(format!("Patched product with ID: {}", self.id))
 //!     }
 //! }
 //!
-//! // Implement handlers for the `Users` struct
-//! #[method_helper(AppState)]
-//! impl Users {
+//! // Implement handlers for the `User` struct
+//! #[method_helper]
+//! impl User {
 //!     #[get]
-//!     async fn get(self) -> Json<String> {
+//!     async fn get_user(self) -> Json<String> {
 //!         println!("User ID: {}", self.id);
 //!         Json(format!("User ID: {}", self.id))
 //!     }
 //!
 //!     #[post]
-//!     async fn create(self) -> Json<String> {
+//!     async fn create_user(self) -> Json<String> {
 //!         println!("Creating user with ID: {}", self.id);
 //!         Json(format!("Created user with ID: {}", self.id))
 //!     }
 //!
 //!     #[put]
-//!     async fn update(self) -> Json<String> {
+//!     async fn update_user(self) -> Json<String> {
 //!         println!("Updating user with ID: {}", self.id);
 //!         Json(format!("Updated user with ID: {}", self.id))
 //!     }
 //!
 //!     #[delete]
-//!     async fn delete(self) -> Json<String> {
+//!     async fn delete_user(self) -> Json<String> {
 //!         println!("Deleting user with ID: {}", self.id);
 //!         Json(format!("Deleted user with ID: {}", self.id))
 //!     }
 //!
 //!     #[patch]
-//!     async fn patch(self) -> Json<String> {
+//!     async fn patch_user(self) -> Json<String> {
 //!         println!("Patching user with ID: {}", self.id);
 //!         Json(format!("Patched user with ID: {}", self.id))
 //!     }
@@ -146,9 +163,10 @@
 //!
 //! #[tokio::main]
 //! async fn main() {
-//!     // Create a Router instance using the `router()`
-//!     // method generated by the [`routes!`] macro
-//!     let r: Router<AppState> = router();
+//!     // Generate the router instance using the `routes()`
+//!     // method provided by the `AllRoutes` type, which is
+//!     // implemented by the `routes!` macro
+//!     let r: Router<AppState> = AllRoutes::routes();
 //!
 //!     let tcp_listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
 //!     let state = AppState;
@@ -159,67 +177,64 @@
 //! For more information, see the [documentation](https://docs.rs/better_routes).
 //! Contributions and feedback are welcome on [GitHub](https://github.com/ratnaraj7/better-routes).
 
-/// A macro for implementing the [`MethodHandler`] trait
-/// with the specified methods and state.
+/// A macro for implementing the [`MethodHandlers`] trait.
 ///
 /// # Example
-/// ```
-/// use better_routes::{routes, method_helper};
-/// use axum_extra::routing::RouterExt;
-/// use axum::Router;
+/// ```rust
+/// use better_routes::method_helper;
+/// use serde::Deserialize;
 ///
-/// #[derive(Clone)]
-/// struct AppState;
+/// #[derive(Deserialize)]
+/// struct Home;
 ///
-/// routes! {
-///     State => AppState,
-///     "/" => struct Home;
-/// }
-///
-/// #[method_helper(AppState)]
+/// #[method_helper]
 /// impl Home {
 ///     #[get]
-///     async fn index(self) {}
+///     async fn get_(self) {}
 ///
 ///     #[post]
-///     async fn create(self) {}
+///     async fn create_(self) {}
 ///
 ///     #[put]
-///     async fn update(self) {}
+///     async fn update_(self) {}
 ///
 ///     #[delete]
-///     async fn delete(self) {}
+///     async fn delete_(self) {}
 ///
 ///     #[patch]
-///     async fn patch(self) {}
-/// }
-///
-/// #[tokio::main]
-/// async fn main() {
-///     // Create a Router instance using the `router()`
-///     // method generated by the `routes!` macro
-///     let r: Router<AppState> = router();
+///     async fn patch_(self) {}
 /// }
 /// ```
 pub use better_routes_macros::method_helper;
 
 /// Define routes using the [`routes!`] macro.
 ///
-/// The [`routes!`] macro generates the `router()` method,
-/// which creates a `Router` instance configured with the
-/// routes defined in the macro. Every typed path used in
-/// the routes must implement the [`MethodHandler`] trait,
+/// The [`routes!`] macro generates a struct, using the name provided, such as `AllRoutes` below,
+/// that implements the `routes()` method. This method creates a
+/// `Router` instance configured with the routes defined in the macro.
+/// You can control the visibility of the generated struct and methods using
+/// visibility modifiers.
+/// Every typed path used in the routes must implement the [`MethodHandlers`] trait,
 /// which is facilitated by the [`method_helper`] macro.
-/// Implementing `MethodHandler` manually is not recommended.
+/// Implementing `MethodHandlers` manually is not recommended.
 ///
 /// # Example
-/// ```
+/// ```rust
 /// use better_routes::{routes, method_helper};
+/// use axum::{
+///     Router,
+///     http::StatusCode
+/// };
 /// use axum_extra::routing::RouterExt;
-/// use axum::Router;
+/// use serde::Deserialize;
 ///
+/// #[derive(Deserialize)]
+/// struct Home;
+///
+/// // Define routes and associate them with handlers
 /// routes! {
-///     "/" => struct Home;
+///     name => pub AllRoutes, // This makes `AllRoutes` public
+///     "/" => Home,
 /// }
 ///
 /// #[method_helper]
@@ -230,27 +245,35 @@ pub use better_routes_macros::method_helper;
 ///
 /// #[tokio::main]
 /// async fn main() {
-///     // Create a Router instance using the `router()`
-///     // method generated by the `routes!` macro
-///     let r: Router = router();
+///     // Create a Router instance using the `routes()`
+///     // method provided by the `AllRoutes` struct
+///     let r: Router = AllRoutes::routes();
 /// }
 /// ```
 ///
 /// # With State
-/// ```
+/// ```rust
 /// use better_routes::{routes, method_helper};
+/// use axum::{
+///     Router
+/// };
 /// use axum_extra::routing::RouterExt;
-/// use axum::Router;
+/// use serde::Deserialize;
+///
+/// #[derive(Deserialize)]
+/// struct Home;
 ///
 /// #[derive(Clone)]
 /// struct AppState;
 ///
+/// // Define routes and specify the application state
 /// routes! {
-///     State => AppState,
-///     "/" => struct Home;
+///     name => AllRoutes,
+///     state => AppState,
+///     "/" => Home
 /// }
 ///
-/// #[method_helper(AppState)]
+/// #[method_helper]
 /// impl Home {
 ///     #[get]
 ///     async fn index(self) {}
@@ -258,23 +281,29 @@ pub use better_routes_macros::method_helper;
 ///
 /// #[tokio::main]
 /// async fn main() {
-///     // Create a Router instance with state using the `router()`
-///     // method generated by the `routes!` macro
-///     let r: Router<AppState> = router();
+///     // Create a Router instance with state using the `routes()`
+///     // method provided by the `AllRoutes` struct
+///     let r: Router<AppState> = AllRoutes::routes();
 /// }
 /// ```
 ///
 /// # With Global Rejection
-/// ```
+/// ```rust
 /// use better_routes::{routes, method_helper};
+/// use axum::{
+///     Router,
+///     http::StatusCode,
+///     response::{IntoResponse, Response},
+///     extract::rejection::PathRejection
+/// };
 /// use axum_extra::routing::RouterExt;
-/// use axum::http::StatusCode;
-/// use axum::response::IntoResponse;
-/// use axum::extract::rejection::PathRejection;
-/// use axum::response::Response;
-/// use axum::Router;
+/// use serde::Deserialize;
 ///
-/// #[derive(Default)]
+/// #[derive(Deserialize)]
+/// struct Home {
+///     id: usize
+/// }
+///
 /// struct GlobalRejection;
 ///
 /// impl From<PathRejection> for GlobalRejection {
@@ -289,9 +318,11 @@ pub use better_routes_macros::method_helper;
 ///     }
 /// }
 ///
+/// // Define routes and specify global rejection handling
 /// routes! {
-///     Rejection => GlobalRejection, // Applied globally if specific rejections are not defined for routes
-///     "/:id" => struct Home { id: usize }
+///     name => AllRoutes,
+///     rejection => GlobalRejection,
+///     "/:id" => Home
 /// }
 ///
 /// #[method_helper]
@@ -302,23 +333,29 @@ pub use better_routes_macros::method_helper;
 ///
 /// #[tokio::main]
 /// async fn main() {
-///     // Create a Router instance using the `router()`
-///     // method generated by the `routes!` macro
-///     let r: Router = router();
+///     // Create a Router instance using the `routes()`
+///     // method provided by the `AllRoutes` struct
+///     let r: Router = AllRoutes::routes();
 /// }
 /// ```
 ///
 /// # With Route-Specific Rejection
-/// ```
+/// ```rust
 /// use better_routes::{routes, method_helper};
+/// use axum::{
+///     Router,
+///     http::StatusCode,
+///     response::{IntoResponse, Response},
+///     extract::rejection::PathRejection
+/// };
 /// use axum_extra::routing::RouterExt;
-/// use axum::http::StatusCode;
-/// use axum::response::IntoResponse;
-/// use axum::extract::rejection::PathRejection;
-/// use axum::response::Response;
-/// use axum::Router;
+/// use serde::Deserialize;
 ///
-/// #[derive(Default)]
+/// #[derive(Deserialize)]
+/// struct Home {
+///     id: usize
+/// }
+///
 /// struct HomeRejection;
 ///
 /// impl From<PathRejection> for HomeRejection {
@@ -333,8 +370,10 @@ pub use better_routes_macros::method_helper;
 ///     }
 /// }
 ///
+/// // Define routes and specify route-specific rejection handling
 /// routes! {
-///     "/:id" => struct Home { id: usize } => HomeRejection // Rejection applies only to this specific route
+///     name => AllRoutes,
+///     "/:id" => Home => HomeRejection
 /// }
 ///
 /// #[method_helper]
@@ -345,9 +384,9 @@ pub use better_routes_macros::method_helper;
 ///
 /// #[tokio::main]
 /// async fn main() {
-///     // Create a Router instance using the `router()`
-///     // method generated by the `routes!` macro
-///     let r: Router = router();
+///     // Create a Router instance using the `routes()`
+///     // method provided by the `AllRoutes` struct
+///     let r: Router = AllRoutes::routes();
 /// }
 /// ```
 pub use better_routes_macros::routes;
