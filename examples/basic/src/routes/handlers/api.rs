b9881062d813a7fd429c86b1a5f0d28b96f2561f
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-10-03 16:12:30 +0000

    chore: update examples

--- examples/basic/src/routes/handlers/api.rs
+++ examples/basic/src/routes/handlers/api.rs
@@ -1,9 +1,11 @@
 use axum::extract::State;
 use axum::Form;
 use axum_extra::routing::TypedPath;
+use origami_engine::Origami;
 use serde::Deserialize;
+use tailwind_fuse::IntoTailwindClass;
 
-use crate::views::Row;
+use crate::views::{row, StatusClass};
 use crate::{AppState, Status};
 
 #[derive(Deserialize)]
@@ -18,7 +20,7 @@ pub async fn create_todo(
     _: Todo,
     State(AppState { db }): State<AppState>,
     Form(CreateTodoPayload { name }): Form<CreateTodoPayload>,
-) -> Row {
+) -> Origami {
     let mut id = 0;
     loop {
         if !db.lock().await.contains_key(&id) {
@@ -32,12 +34,13 @@ pub async fn create_todo(
         .entry(id)
         .or_insert((name, Status::Todo))
         .clone();
-    Row {
-        id,
-        name,
-        status,
-        delete_todo_url: TodoWithId { id }.to_uri().to_string(),
-        update_todo_url: TodoWithId { id }.to_uri().to_string(),
+    row! {
+        id { @(format!("row-{}", id).as_str()) },
+        status { @(StatusClass { status }.to_class().as_str()) },
+        name { *name.as_str(); },
+        delete_todo_url { @(TodoWithId {id}.to_uri().to_string().as_str()) },
+        target { @(format!("#row-{}", id).as_str()) },
+        onclick { @(format!("show_dialog('#row-{}', '{}', '{}', '{}')", id, TodoWithId { id }.to_uri(), name, status).as_str()) }
     }
 }
 
@@ -56,15 +59,16 @@ pub async fn update_todo(
     TodoWithId { id }: TodoWithId,
     State(AppState { db }): State<AppState>,
     Form(UpdateTodoPayload { name, status }): Form<UpdateTodoPayload>,
-) -> Row {
+) -> Origami {
     *db.lock().await.get_mut(&id).unwrap() = (name.clone(), status);
 
-    Row {
-        id,
-        name,
-        status,
-        delete_todo_url: TodoWithId { id }.to_uri().to_string(),
-        update_todo_url: TodoWithId { id }.to_uri().to_string(),
+    row! {
+        id { @(format!("row-{}", id).as_str()) },
+        status { @(StatusClass { status }.to_class().as_str()) },
+        name { *name.as_str(); },
+        delete_todo_url { @(TodoWithId {id}.to_uri().to_string().as_str()) },
+        target { @(format!("#row-{}", id).as_str()) },
+        onclick { @(format!("show_dialog('#row-{}', '{}', '{}', '{}')", id, TodoWithId { id }.to_uri(), name, status).as_str()) }
     }
 }
 
--- examples/basic/src/views.rs
+++ examples/basic/src/views.rs
@@ -1,22 +1,19 @@
 use std::fmt::Display;
 
-use origami_engine::{og, Layout, Origami};
+use origami_engine::comp;
 use serde::Deserialize;
 use tailwind_fuse::*;
 
-struct Main;
-
-og! {
-    layout MainLayout,
-    Main =>
+comp! {
+    layout =>
     "<!DOCTYPE html>"!
     html {
         head {
-            block head;
+            $head
             script src="https://unpkg.com/htmx.org@2.0.1" integrity="sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/" crossorigin="anonymous" {}
             script src="https://cdn.tailwindcss.com" {}
             style {
-                "* {scrollbar-width: thin; scrollbar-color: #9CA3AF #1f2937;} .section { max-width: 1024px; margin: 0 auto; min-width: 375px }"!
+                "* {scrollbar-width: thin; scrollbar-color: #9CA3AF #1f2937;} .section { max-width: 1024px; margin: 0 auto; min-width: 375px }"
             }
             script {
                 r#"
@@ -26,100 +23,104 @@ og! {
                         }
                       }
                     }
-                  "#!
+                  "#
             }
         }
         body class="min-h-screen bg-slate-900 px-10 py-10 flex" {
-            block body;
+            $body
         }
     }
 }
+pub(crate) use layout;
 
-pub struct Home {
-    pub rows: Vec<Row>,
-    pub create_todo_url: String,
-}
-
-og! {
-    @Main{} MainLayout,
-    Home =>
-    extend head {
-        title {
-            "TODO"
-        }
-    }
-    extend body {
-        div class="m-auto rounded-md bg-slate-800 p-10 space-y-4" {
-            h1 class="text-3xl font-bold text-white" {
+comp! {
+    home =>
+    @layout! {
+        head {
+            title {
                 "TODO"
             }
-            div class="space-y-8" {
-                form "hx-target"="#rows" "hx-swap"="beforeend" "hx-post"=(self.create_todo_url.as_str()) class="space-y-4" {
-                    input "type"="text" required autocomplete="off" name="name" placeholder="Name" class="w-full px-4 py-2 rounded-md bg-slate-700 text-white focus:outline-none";
-                    button "type"="submit" class="w-full px-4 py-2 rounded-md bg-slate-300 text-slate-800 focus:outline-none" {
-                        "Add"
-                    }
+        },
+        body {
+            div class="m-auto rounded-md bg-slate-800 p-10 space-y-4" {
+                h1 class="text-3xl font-bold text-white" {
+                    "TODO"
                 }
-                table class="w-[60rem] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" {
-                    @RowHead{};
-                    tbody id="rows" {
-                        @(..self.rows.iter());
+                div class="space-y-8" {
+                    form "hx-target"="#rows" "hx-swap"="beforeend" "hx-post"=$create_todo_url class="space-y-4" {
+                        input "type"="text" required autocomplete="off" name="name" placeholder="Name" class="w-full px-4 py-2 rounded-md bg-slate-700 text-white focus:outline-none";
+                        button "type"="submit" class="w-full px-4 py-2 rounded-md bg-slate-300 text-slate-800 focus:outline-none" {
+                            "Add"
+                        }
                     }
-                }
-                dialog class="h-screen w-full bg-black/40" {
-                    div class="w-full h-full flex" {
-                        form id="dialog-form" class="m-auto w-[30rem] p-4 bg-slate-800 rounded-md space-y-4" {
-                            input id="dialog-input" "type"="text" required autocomplete="off" name="name" placeholder="Name" class="w-full px-4 py-2 rounded-md bg-slate-700 text-white focus:outline-none";
-                            select id="dialog-select" class="w-full px-4 py-2 rounded-md bg-slate-700 text-white focus:outline-none" name="status" {
-                                option "value"="Todo" {
-                                    "Todo"
+                    table class="w-[60rem] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" {
+                        @row_head!{};
+                        tbody id="rows" {
+                            for row in $rows.iter(); {
+                                @row!{
+                                    id { @(format!("row-{}", row.id).as_str()) },
+                                    status { @(StatusClass { status: row.status }.to_class().as_str()) },
+                                    name { *row.name.as_str(); },
+                                    delete_todo_url { @(row.delete_todo_url.as_str()) },
+                                    target { @(format!("#row-{}", row.id).as_str()) },
+                                    onclick { @(format!("show_dialog('#row-{}', '{}', '{}', '{}')", row.id, row.update_todo_url, row.name, row.status).as_str()) }
+                                };
+                            }
+                        }
+                    }
+                    dialog class="h-screen w-full bg-black/40" {
+                        div class="w-full h-full flex" {
+                            form id="dialog-form" class="m-auto w-[30rem] p-4 bg-slate-800 rounded-md space-y-4" {
+                                input id="dialog-input" "type"="text" required autocomplete="off" name="name" placeholder="Name" class="w-full px-4 py-2 rounded-md bg-slate-700 text-white focus:outline-none";
+                                select id="dialog-select" class="w-full px-4 py-2 rounded-md bg-slate-700 text-white focus:outline-none" name="status" {
+                                    option "value"="Todo" {
+                                        "Todo"
+                                    }
+                                    option "value"="Done" {
+                                        "Done"
+                                    }
                                 }
-                                option "value"="Done" {
-                                    "Done"
+                                button "type"="submit" class="w-full px-4 py-2 rounded-md bg-slate-300 text-slate-800 focus:outline-none" {
+                                    "Update"
                                 }
-                            }
-                            button "type"="submit" class="w-full px-4 py-2 rounded-md bg-slate-300 text-slate-800 focus:outline-none" {
-                                "Update"
-                            }
-                            button "type"="button" onclick="hide_dialog()" class="w-full px-4 py-2 rounded-md bg-red-500 text-slate-800 focus:outline-none" {
-                                "Close"
+                                button "type"="button" onclick="hide_dialog()" class="w-full px-4 py-2 rounded-md bg-red-500 text-slate-800 focus:outline-none" {
+                                    "Close"
+                                }
                             }
                         }
                     }
-                }
-                script {
-                    r#"
-                        function show_dialog(id, update_url, name, status) {
-                            document.querySelector("dialog").showModal();
-                            document.getElementById("dialog-input").value = name;
-                            document.getElementById("dialog-select").querySelector(`option[value="${status}"]`).selected = true;
-                            document.getElementById("dialog-form").onsubmit = function(evt) {
-                                evt.preventDefault();
-                                const values = htmx.values(htmx.find('#dialog-form'));
-                                htmx.ajax('PUT', update_url, {
-                                    target: id,
-                                    swap: "outerHTML",
-                                    values
-                                });
-                                hide_dialog();
-                            };
-                        }
-                        
-                        function hide_dialog() {
-                            document.querySelector("dialog").close();
-                        }
-                    "#!
+                    script {
+                        r#"
+                            function show_dialog(id, update_url, name, status) {
+                                document.querySelector("dialog").showModal();
+                                document.getElementById("dialog-input").value = name;
+                                document.getElementById("dialog-select").querySelector(`option[value="${status}"]`).selected = true;
+                                document.getElementById("dialog-form").onsubmit = function(evt) {
+                                    evt.preventDefault();
+                                    const values = htmx.values(htmx.find('#dialog-form'));
+                                    htmx.ajax('PUT', update_url, {
+                                        target: id,
+                                        swap: "outerHTML",
+                                        values
+                                    });
+                                    hide_dialog();
+                                };
+                            }
 
+                            function hide_dialog() {
+                                document.querySelector("dialog").close();
+                            }
+                        "#
+                    }
                 }
             }
         }
-    }
+    };
 }
+pub(crate) use home;
 
-pub struct RowHead;
-
-og! {
-    RowHead =>
+comp! {
+    row_head =>
     thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400" {
         tr {
             th scope="col" class="px-6 py-3 w-10/12" {
@@ -131,6 +132,7 @@ og! {
         }
     }
 }
+pub(crate) use row_head;
 
 #[derive(TwClass)]
 #[tw(class = "px-6 py-4 font-medium text-gray-900")]
@@ -163,20 +165,21 @@ pub struct Row {
     pub update_todo_url: String,
 }
 
-og! {
-    Row =>
-    tr id=(format!("row-{}", self.id).as_str()) class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" {
+comp! {
+    row =>
+    tr id=$id class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" {
         th scope="row"
-            class=(StatusClass { status: self.status }.to_class().as_str()) {
-            (self.name.as_str());
+            class=$status {
+                $name
         }
         td class="px-6 py-4 space-x-2" {
-            a class="decoration-wavy underline text-red-500 cursor-pointer" "hx-delete"=(self.delete_todo_url.as_str()) "hx-target"=(format!("#row-{}", self.id).as_str()) "hx-swap"="outerHTML" {
+            a class="decoration-wavy underline text-red-500 cursor-pointer" "hx-delete"=$delete_todo_url "hx-target"=$target "hx-swap"="outerHTML" {
                 "Delete"
             }
-            a class="decoration-wavy underline text-slate-500 cursor-pointer" onclick=(format!("show_dialog('#row-{}', '{}', '{}', '{}')", self.id, self.update_todo_url, self.name, self.status).as_str()) {
+            a class="decoration-wavy underline text-slate-500 cursor-pointer" onclick=$onclick {
                 "Update"
             }
         }
     }
 }
+pub(crate) use row;
